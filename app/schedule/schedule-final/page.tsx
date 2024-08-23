2421466676b80a674391e36d8541ac5758fe23ac
lonnyrichard27 <lonnyrichard77@gmail.com>
2024-08-23 02:13:31 +0100

    up

--- app/schedule/schedule-final/page.tsx
+++ app/schedule/schedule-final/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation';
 import CustomButton from '@/components/Custom/CustomButton';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -8,56 +8,54 @@ import React, { Suspense, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { verify } from '@/api/user';
 
-
 const page = () => {
-//   const searchParams = useSearchParams()
- 
-//   const search = searchParams.get('reference')
+    const searchParams = useSearchParams()
 
-//   const { data: viewReq, isLoading, error } = useQuery({
-//     queryKey: ['getRef'],
-//     queryFn: () => verify(search)
-//   });
+    const search = searchParams.get('reference')
+    console.log(search)
 
-// console.log(viewReq, 'the data')
+  //   const { data: viewReq, isLoading, error } = useQuery({
+  //     queryKey: ['getRef'],
+  //     queryFn: () => verify(search)
+  //   });
 
+  // console.log(viewReq, 'the data')
 
   return (
     <Suspense>
-
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <section className="border md:w-[368px] rounded-md p-5 grid items-center justify-center">
-        <Image
-          src="/images/success.png"
-          alt="nav logo"
-          width={100}
-          height={100}
-          className="w-full"
-        />
-        <article className="text-center">
-          <p className="mt-16">Success!</p>
-          <p>
-            Your appointment schedule has been
-            <br /> set successfully. Kindly print your
-            <br /> appointment slip below.
-          </p>
-        </article>
-        <Link href="/schedule/schedule-form-3" className="mt-16">
-          <CustomButton
-            text="Print Appointment slip"
-            className="w-full mt-3 py-3"
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <section className="border md:w-[368px] rounded-md p-5 grid items-center justify-center">
+          <Image
+            src="/images/success.png"
+            alt="nav logo"
+            width={100}
+            height={100}
+            className="w-full"
           />
-        </Link>
-        <Link href="/schedule/schedule-form-3" className="mt-2">
-          <CustomButton
-            text="Go To Home"
-            className="w-full mt-3 py-3"
-            bgColor="bg-[#F2F4F7]"
-            color="text-black"
-          />
-        </Link>
-      </section>
-    </main>
+          <article className="text-center">
+            <p className="mt-16">Success!</p>
+            <p>
+              Your appointment schedule has been
+              <br /> set successfully. Kindly print your
+              <br /> appointment slip below.
+            </p>
+          </article>
+          <Link href="/schedule/schedule-form-3" className="mt-16">
+            <CustomButton
+              text="Print Appointment slip"
+              className="w-full mt-3 py-3"
+            />
+          </Link>
+          <Link href="/schedule/schedule-form-3" className="mt-2">
+            <CustomButton
+              text="Go To Home"
+              className="w-full mt-3 py-3"
+              bgColor="bg-[#F2F4F7]"
+              color="text-black"
+            />
+          </Link>
+        </section>
+      </main>
     </Suspense>
   );
 };
