7e2fa99c321a8886134a3c50a3f0750d4e5838e2
Peter Okerulu <oduchep@gmail.com>
2024-09-19 14:48:13 +0100

    verify certificate

--- services/certificate-services.ts
+++ services/certificate-services.ts
@@ -3,7 +3,7 @@ import {
   PaginationData,
   PaginationData2,
 } from '@/types/ApiResponse';
-import { CertificateType } from '@/types/CertificateType';
+import { CertificateType, VerifiedCertTypes } from '@/types/CertificateType';
 import { DeliveryTypes } from '@/types/DeliveryTypes';
 import axiosInstance from '@/util/axios';
 import { handleError } from '@/util/errorHandler';
@@ -39,6 +39,21 @@ export const getOngoingDeliveries = async () => {
     const response = await axiosInstance.get<
       ApiResponse<PaginationData2<DeliveryTypes>>
     >(`/delivery/fetch-delivery-requests`);
+    return response.data.data;
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getVerifiedCertificate = async ({
+  id,
+}: {
+  id: string | string[];
+}) => {
+  try {
+    const response = await axiosInstance.get<ApiResponse<VerifiedCertTypes>>(
+      `/biodata/public-profile/${id}`
+    );
     return response.data.data;
   } catch (error) {
     handleError(error);
