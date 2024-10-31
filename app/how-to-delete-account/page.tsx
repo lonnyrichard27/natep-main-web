d9431b7e312cfac38153e77cced26b1a2ad9f731
Peter Okerulu <oduchep@gmail.com>
2024-10-31 10:20:46 +0100

    how to delete account

--- app/how-to-delete-account/page.tsx
+++ app/how-to-delete-account/page.tsx
@@ -1,28 +1,73 @@
 'use client';
 
 import { Navbar, Footer } from '@/components/Navigation';
+import { DeleteAccount1, DeleteAccount2 } from '@/public/images';
+import Image from 'next/image';
 import React from 'react';
 
 const DeleteAccountPage = () => {
+  const types_of_data = [
+    'Personal information (name, email address, phone number)',
+    'Transaction history',
+    'Any other personal information you have provided to us.',
+  ];
+
   return (
     <>
       <Navbar />
 
       <div className='bg-background-gray py-20'>
-        <div className='mx-auto flex flex-col gap-12 bg-white p-10 text-sm !font-light lg:w-[720px]'>
+        <div className='mx-auto flex flex-col gap-7 bg-white p-10 text-sm !font-light leading-6 lg:w-[720px]'>
           <div>
             <h1 className='mb-2 text-lg font-semibold text-[#101828]'>
-              Privacy Policy for the National Talent Export Programme (NATEP)
-              App
+              How to Delete your Account on NTEP Mobile App
             </h1>
-            <p className='text-[#344054]'>Effective Date: 01 JUL 2024</p>
+            <p className='text-[#344054]'>Last Updated: 01 JUL 2024</p>
+          </div>
 
-            <p className='mt-6'>
-              At NATEP, we are committed to safeguarding your personal data and
-              ensuring your privacy. This Privacy Policy outlines how we
-              collect, use, protect, and share your information in compliance
-              with the latest data protection regulations, including principles
-              of privacy by design and privacy by default.
+          <div className='flex flex-col gap-3'>
+            <p>
+              We take your privacy seriously. If you would like to request that
+              your data be deleted, please follow these simple steps:
+            </p>
+            <p>- Launch App</p>
+            <p>- Navigate to the More menu</p>
+          </div>
+
+          <Image
+            src={DeleteAccount1}
+            width={173}
+            height={375}
+            alt='mobile app image'
+          />
+
+          <div className='flex flex-col gap-3'>
+            <p>- Tap on the Delete Account button</p>
+            <p>- Confirm that you would like to delete your Account / Data</p>
+          </div>
+
+          <Image
+            src={DeleteAccount2}
+            width={173}
+            height={375}
+            alt='mobile app image'
+          />
+
+          <div className='flex flex-col gap-3'>
+            <p>
+              NATEP App deletes the following types of data when you request
+              that your Account be deleted:
+            </p>
+
+            <ul className='flex list-decimal flex-col gap-1 pl-5'>
+              {types_of_data?.map((item, index) => <li key={index}>{item}</li>)}
+            </ul>
+
+            <p>
+              Please note that we may be required to retain certain data for
+              legal or regulatory purposes, but we will delete your personal
+              information to the extent possible and in accordance with
+              applicable laws and regulations.
             </p>
           </div>
         </div>
---
+++ public/images/index.ts
@@ -0,0 +1,4 @@
+import DeleteAccount1 from './delete_account_1.png';
+import DeleteAccount2 from './delete_account_2.png';
+
+export { DeleteAccount1, DeleteAccount2 };
